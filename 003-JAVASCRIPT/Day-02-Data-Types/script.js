24cf3e09f395c97c50ab48713c54cdc95cb66304
Abhijit <rabha3222@gmail.com>
2026-07-16 19:28:42 +0530

    added JS file for data types

---
+++ 003-JAVASCRIPT/Day-02-Data-Types/script.js
@@ -0,0 +1,49 @@
+//STRING
+
+let name = "Abhijit";
+
+//NUMBER
+
+let age = 22;
+
+//BOOLEAN
+
+let isStudent = false;
+
+//UNDEFINDED
+
+let city;
+
+//NULL
+
+let car = null;
+
+//BigInt
+
+let population = 987654321123456789n;
+
+//SYMBOL
+
+let id = Symbol("user");
+
+//PRINT VALUES
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(city);
+console.log(car);
+console.log(population);
+console.log(id);
+
+console.log("-------------");
+
+//TYPEOF
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof city);
+console.log(typeof car);
+console.log(typeof population);
+console.log(typeof id);
