8a97372cb09ed4f6d78caa27350ce59876d2dc43
Abhijit <rabha3222@gmail.com>
2026-07-16 18:51:37 +0530

    added script.js for day 1

--- 003-JAVASCRIPT/Day-01-Introduction/script.js
+++ 003-JAVASCRIPT/Day-01-Introduction/script.js
@@ -1,3 +1,11 @@
 console.log("Welcome to JavaScript");
 console.log("My Name is Abhijit");
 console.log("Subscribe my YT channle Abhijit Rava Vlogs");
+
+let name ="Abhijit";
+let age = 22;
+const country = "India";
+
+console.log(name);
+console.log(age);
+console.log(country);
