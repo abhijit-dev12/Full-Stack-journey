c265d59af588c91e3dfd8231ffba625f27a3e6c7
Abhijit <rabha3222@gmail.com>
2026-07-16 19:42:21 +0530

    Mini project for day 2 Data Types Done

---
+++ 003-JAVASCRIPT/Day-02-Data-Types/Day-02-Mini-Project/README.md
@@ -0,0 +1,20 @@
+Name
+Age
+City
+Favorite Game
+Is Employed
+Dream Bike
+Current Salary
+
+
+# Print each value followed by its data type.
+
+Example:
+Name: Abhijit
+Type: string
+
+Age: 22
+Type: number
+
+Is Employed: true
+Type: boolean
---
+++ 003-JAVASCRIPT/Day-02-Data-Types/Day-02-Mini-Project/script.js
@@ -0,0 +1,27 @@
+let name = "Abhijit";
+console.log(name);
+console.log(typeof name);
+
+let age = 22;
+console.log(age);
+console.log(typeof age);
+
+let city = "Bengaluru";
+console.log(city);
+console.log(typeof city);
+
+let favGame = "BGMI";
+console.log(favGame);
+console.log(typeof favGame);
+
+let isEmployed = true;
+console.log(isEmployed);
+console.log(typeof isEmployed);
+
+let dreamBike = "Ducati";
+console.log(dreamBike);
+console.log(typeof dreamBike);
+
+let salary = 20000;
+console.log(salary);
+console.log(typeof salary);
