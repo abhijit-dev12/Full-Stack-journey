2cd5c1fae58a8f96cb92fa88d6c45155484ab80f
Abhijit <rabha3222@gmail.com>
2026-07-17 11:13:54 +0530

    added day 3 html and js file

---
+++ 003-JAVASCRIPT/Day-03-Operators/script.js
@@ -0,0 +1,46 @@
+let num1 = 20;
+let num2 = 5;
+
+console.log("Arithmetic: ",num1 +num2);
+console.log("Subtraction: ", num1-num2);
+console.log("Multiplication: ", num1*num2);
+console.log("Divisio: ",num1/num2);
+console.log("Modulas: ", num1 % num2);
+console.log("Exponent: ", num1 ** num2);
+
+console.log("----------------------");
+
+
+let score = 10;
+
+score += 5;
+console.log("After += :", score);
+
+score -= 3;
+console.log("After -=: ",score);
+
+score *= 2;
+console.log("After *=: ",score);
+
+score /= 4;
+console.log("After /=: ",score);
+
+console.log("----------------------");
+
+
+let isStudent = true;
+let hasLaptop = false;
+
+console.log("AND: ", isStudent && hasLaptop);
+console.log("OR: ", isStudent || hasLaptop);
+console.log("NOT: ", !isStudent);
+
+console.log("---------------------------");
+
+let count = 1;
+
+count++;
+console.log("Increment: ",count);
+
+count--;
+console.log("Decrement: ", count);
