c16819886ad487db9afce5f6a390dad4ca27d0cf
Abhijit <rabha3222@gmail.com>
2026-07-18 19:58:00 +0530

    added if,if else and else if code

---
+++ 003-JAVASCRIPT/Day-04-Conditional-Statements/script.js
@@ -0,0 +1,39 @@
+// if statements
+
+let age = 20;
+
+if(age>20) {
+    console.log("You are skibidi. ");
+}
+
+console.log("------------");
+
+// if else statement
+
+let marks = 35;
+
+if(marks>= 40){
+    console.log("Pass");
+} else {
+    console.log("fail");
+}
+
+console.log("-------------");
+
+//else if
+
+let score = 67;
+
+if(score>=67){
+    console.log("Grade A");
+} else if(score>=57){
+    console.log("Grade B");
+} else if(score>=47){
+    console.log("Grade C");
+} else {
+    console.log("Grade E");
+}
+
+
+console.log("---------------");
+
