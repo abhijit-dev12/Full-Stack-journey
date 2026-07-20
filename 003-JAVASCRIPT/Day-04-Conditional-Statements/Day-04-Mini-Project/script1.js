c88a7554d169cce67788d2129c1cc5798d526828
Abhijit <rabha3222@gmail.com>
2026-07-20 19:39:49 +0530

    mini project 1 done

---
+++ 003-JAVASCRIPT/Day-04-Conditional-Statements/Day-04-Mini-Project/script1.js
@@ -0,0 +1,11 @@
+let age = 22;
+
+let hasLicense = true;
+
+let hasHelmet = true;
+
+if(age >= 18 && hasLicense && hasHelmet ) {
+    console.log("You can Ride Bike");
+} else {
+    console.log("Get a helmet");
+}
